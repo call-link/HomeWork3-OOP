6a165205f22f78aa03b271d4f143ee1c583092b4
Hadi Mohammadi <132592564+call-link@users.noreply.github.com>
2025-12-27 12:49:34 +0330

    task 4 ended

---
+++ Task-04/Models/Patient.cs
@@ -0,0 +1,24 @@
+﻿namespace HospitalManagement.Models;
+
+public class Patient : Person
+{
+    public int PatientId { get; set; }
+    public List<string> MedicalHistory { get; }
+
+    public Patient(string name, int age, string nationalId, int patientId)
+        : base(name, age, nationalId)
+    {
+        PatientId = patientId;
+        MedicalHistory = new List<string>();
+    }
+
+    public void AddToMedicalHistory(string disease)
+    {
+        MedicalHistory.Add(disease);
+    }
+
+    public override string GetDetails()
+    {
+        return base.GetDetails() + $", PatientId: {PatientId}";
+    }
+}
---
+++ Task-04/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace HospitalManagement.Models;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string NationalId { get; set; }
+
+    public Person(string name, int age, string nationalId)
+    {
+        Name = name;
+        Age = age;
+        NationalId = nationalId;
+    }
+
+    public virtual string GetDetails()
+    {
+        return $"Name: {Name}, Age: {Age}, NationalId: {NationalId}";
+    }
+}
